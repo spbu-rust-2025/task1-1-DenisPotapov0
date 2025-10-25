0bed3c9bf6dd4471f8815e71c07a7b9745d77a97
Denis <daerexq@yande.ru>
2025-10-25 20:05:40 +0300

    Add main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
-fn main() {
 
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).expect("Не удалось считать строку");
+    let mut parts = input.split_whitespace();
+    let a: i32 = parts.next().expect("Нет первого числа").parse().expect("Некорректное число");
+    let b: i32 = parts.next().expect("Нет второго числа").parse().expect("Некорректное число");
+
+    println!("{}", a + b);
 }
